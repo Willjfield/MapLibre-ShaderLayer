3d68533cd31d1cc7e145414f1d65625be789ee67
Will Field <wfield@gc.cuny.edu>
2024-08-01 12:04:21 -0400

    dirt and earth

--- crunchyworld/frag.glsl
+++ crunchyworld/frag.glsl
@@ -12,9 +12,38 @@ uniform sampler2D u_normal;
 out highp vec4 fragColor;
 uniform float u_zoom;
 uniform vec3 u_camera;
+uniform highp vec2 u_resolution;
 
+uniform vec4 u_bbox;
+uniform float u_devicePixelRatio;
+
+vec2 map(vec2 value, vec2 inMin, vec2 inMax, vec2 outMin, vec2 outMax) {
+  return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
+}
+
+
+highp vec2 normalizedLatLngFromFragCoord() {
+
+  highp vec2 uv = gl_FragCoord.xy/u_resolution;
+
+  return map(uv, u_bbox.xy, u_bbox.zw, vec2(0.), vec2(u_devicePixelRatio));
+
+}
     void main(void) {
-      float _pow = u_zoom > 7. ? 3. : 2.;
+      float _pow = u_zoom > 7. ? 3.5 : 2.;
       float _zoom = max(pow(u_zoom*2.,_pow),1.);
-      fragColor = texture(u_texture, v_texcoord*_zoom);
+      vec3 fragWorldPosition = vec3(normalizedLatLngFromFragCoord(),0.);
+      //Get gl_FragCoord's latlng
+      //Normalize to 0-1 to match camera's position
+      //Then get the vec from fragment to camera
+      highp vec2 uv = gl_FragCoord.xy/u_resolution;
+
+      highp vec3 uvwvec = normalize(vec3((uv.xy*2.-1.5),-.15)-u_camera.xyz);
+
+      vec3 normSample = texture(u_normal, v_texcoord*_zoom).xyz;
+      float dotCamera = dot(uvwvec,normSample.xyz)*-2.;
+      
+      vec4 rgb = texture(u_texture, v_texcoord*_zoom)*dotCamera;
+      //float dotPos = dot(u_camera.xzy,normSample.xyz);
+      fragColor = vec4(rgb.xyz,1.);
     }
--- crunchyworld/main.js
+++ crunchyworld/main.js
@@ -17,7 +17,9 @@ const map = new maplibre.Map({
 });
 
 let shaderLayer;
-
+let pavementShader;
+let u_resolutionLocation, u_pixelRatio;
+let pu_resolutionLocation, pu_pixelRatio;
 map.once('load', () => {
   shaderLayer = new ShaderLayer(map, 'shaderLayer', ['landuse_park'], {
     fragmentSource: frag,
@@ -25,16 +27,51 @@ map.once('load', () => {
     imagePath: './crunchyworld/Textures/Dirtx256.png',
     normalImagePath: './crunchyworld/Textures/DirtNorm.png'
   });
+  pavementShader = new ShaderLayer(map, 'pavementShader', ['earth'], {
+    fragmentSource: frag,
+    vertexSource: vert,
+    imagePath: './crunchyworld/Textures/Pavementx256.png',
+    normalImagePath: './crunchyworld/Textures/PavementNormx256.png'
+  });
   map.addLayer(shaderLayer, 'landuse_urban_green');
+  map.addLayer(pavementShader, 'shaderLayer');
+
   const gl = shaderLayer.context;
   const prog = shaderLayer.program;
+
+  const pavementShadergl = pavementShader.context;
+  const pavementShaderprog = pavementShader.program;
 
   gl.useProgram(prog);
   const u_zoom_location = gl.getUniformLocation(prog, 'u_zoom');
   gl.uniform1f(u_zoom_location, Math.floor(map.getZoom()));
+
+  pavementShadergl.useProgram(pavementShaderprog);
+  const pu_zoom_location = gl.getUniformLocation(pavementShaderprog, 'u_zoom');
+  gl.uniform1f(pu_zoom_location, Math.floor(map.getZoom() * -.1));
+
   console.log(gl.getProgramInfoLog(prog))
 
 });
+
+function updateResolution() {
+  const gl = shaderLayer.context;
+  const prog = shaderLayer.program;
+  gl.useProgram(prog);
+  u_resolutionLocation = gl.getUniformLocation(prog, 'u_resolution');
+  gl.uniform2fv(u_resolutionLocation, [map.getContainer().offsetWidth, map.getContainer().offsetHeight]);
+  u_pixelRatio = gl.getUniformLocation(prog, 'u_devicePixelRatio');
+  gl.uniform1f(u_pixelRatio, window.devicePixelRatio);
+
+  const pgl = pavementShader.context;
+  const pprog = pavementShader.program;
+  pgl.useProgram(pprog);
+  pu_resolutionLocation = pgl.getUniformLocation(pprog, 'u_resolution');
+  pgl.uniform2fv(pu_resolutionLocation, [map.getContainer().offsetWidth, map.getContainer().offsetHeight]);
+  pu_pixelRatio = pgl.getUniformLocation(pprog, 'u_devicePixelRatio');
+  pgl.uniform1f(pu_pixelRatio, window.devicePixelRatio);
+
+}
 
 map.on('move', () => {
 
@@ -48,8 +85,25 @@ map.on('move', () => {
   const u_camera_location = gl.getUniformLocation(prog, 'u_camera');
   const x = (map.getCenter().lng + 180) / 360;
   const y = (map.getCenter().lat + 90) / 180;
-  //console.log([x, y, 1 - (map.getZoom() / 20)])
+  shaderLayer.updateMapBBox();
   gl.uniform3f(u_camera_location, x, y, 1 - (map.getZoom() / 20));
+
+  const pgl = pavementShader.context;
+  const pprog = pavementShader.program;
+  pgl.useProgram(pprog);
+
+  const pu_zoom_location = gl.getUniformLocation(pprog, 'u_zoom');
+  pgl.uniform1f(pu_zoom_location, Math.floor(map.getZoom()));
+
+  const pu_camera_location = pgl.getUniformLocation(pprog, 'u_camera');
+  const px = (map.getCenter().lng + 180) / 360;
+  const py = (map.getCenter().lat + 90) / 180;
+  pavementShader.updateMapBBox();
+  pgl.uniform3f(pu_camera_location, px, py, 1 - (map.getZoom() / 20));
+
+  updateResolution();
+  //console.log([x, y, 1 - (map.getZoom() / 20)])
+
 });
 
 
